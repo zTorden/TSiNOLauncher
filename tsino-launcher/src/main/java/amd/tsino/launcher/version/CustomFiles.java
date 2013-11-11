76f7a64fd6d8699b53963daa490f7d01e1251e27
Vadzim Dambrouski <pftbest@gmail.com>
2013-11-11 21:42:39 +0300

    added servers.dat extraction code

--- tsino-launcher/src/main/java/amd/tsino/launcher/version/CustomFiles.java
+++ tsino-launcher/src/main/java/amd/tsino/launcher/version/CustomFiles.java
@@ -1,11 +1,14 @@
 package amd.tsino.launcher.version;
 
+import amd.tsino.bootstrap.EtagDatabase;
 import amd.tsino.launcher.LauncherConstants;
 import amd.tsino.launcher.LauncherUtils;
 import amd.tsino.launcher.download.DownloadJob;
 import amd.tsino.launcher.download.Downloadable;
 import net.minecraft.launcher.Launcher;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
@@ -28,6 +31,15 @@ public class CustomFiles implements ArtifactList {
         try {
             LauncherUtils.unzip(rcpackZip.getFile(), LauncherUtils.getFile(LauncherConstants.RCPACKS_BASE), null);
         } catch (IOException e) {
+            Launcher.getInstance().getLog().error(e);
+        }
+        try {
+            String name = "servers.dat";
+            File file = LauncherUtils.getFile(name);
+            if (!file.exists()) {
+                EtagDatabase.copyAndDigest(getClass().getResourceAsStream("/" + name), new FileOutputStream(file));
+            }
+        } catch (Exception e) {
             Launcher.getInstance().getLog().error(e);
         }
     }
